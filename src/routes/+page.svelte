a8abd157b8b79335e4ce960592fa66a629a3f813
ReneP8 <Rene-Baving@web.de>
2023-02-28 16:19:13 +0100

    added basic layout and randomize feature

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,46 @@
-<div class="flex flex-col items-center justify-center h-screen">
-	<p class="m-3">Enter all your choices and click on Generate to get random a choice.</p>
-	<input class="input w-[50%] p-2" placeholder="Type choice" />
-</div>
+<script lang="ts">
+	let choices: string[] = ['', ''];
+	let result: string;
+
+	function addChoice() {
+		choices = [...choices, ''];
+	}
+
+	function deleteChoice(index: number) {
+		choices.splice(index, 1);
+		choices = choices;
+	}
+
+	function getRandomChoice() {
+		let valid = choices.filter((element) => element);
+		result = valid[Math.floor(Math.random() * valid.length)];
+	}
+</script>
+
+<main>
+	<div class="flex flex-col items-center gap-20 mt-5 h-full">
+		<div class="flex flex-col items-center w-[60%] gap-10 mt-10">
+			{#if result}
+				<h1>{result}</h1>
+			{:else}
+				<h1>No Result</h1>
+			{/if}
+			<button class="btn bg-secondary-500 w-full" on:click|preventDefault={getRandomChoice}
+				><span>Roll</span></button
+			>
+		</div>
+
+		<div class="flex flex-col items-center w-[60%] gap-5 mb-5">
+			<p>Enter all your choices and click on Generate to get random a choice.</p>
+			<button class="btn bg-primary-500 p-2 w-full" on:click|preventDefault={addChoice}
+				><span>Add Choice</span></button
+			>
+			{#each choices as choice, i}
+				<div class="input-group input-group-divider grid-cols-[1fr_auto]">
+					<input class="input p-2" bind:value={choice} placeholder="Choice {i + 1}" />
+					<button class="variant-filled-error" on:click={() => deleteChoice(i)}>Delete</button>
+				</div>
+			{/each}
+		</div>
+	</div>
+</main>
